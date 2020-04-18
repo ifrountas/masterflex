fe61ddc8d91014b3533142f38aac98ebb7713c4c
Iakovos Frountas <iakovosfrountas@gmail.com>
2020-04-18 21:47:41 +0300

    add slideshow

---
+++ src/assets/js/function.slideshow.js
@@ -0,0 +1,21 @@
+import Siema from 'siema';
+
+const mySiema = new Siema({
+    selector: '.siema',
+    duration: 200,
+    easing: 'ease-out',
+    perPage: 1,
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: false,
+    rtl: false,
+    onInit: () => {},
+    onChange: () => {},
+});
+
+document.querySelector('.slide-prev').addEventListener('click', () => mySiema.prev());
+document.querySelector('.slide-next').addEventListener('click', () => mySiema.next());
+
+//https://codepen.io/collection/Adpkkd/?cursor=ZD0xJm89MCZwPTEmdj0xMDIyNDU0

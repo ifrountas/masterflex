d1bcd6c0f4569008c2bf9f683e526e161d16c060
Iakovos Frountas <iakovosfrountas@gmail.com>
2020-11-10 20:57:51 +0200

    Adding changes

--- inc/config.php
+++ inc/config.php
@@ -1,21 +1,21 @@
 <?php
 
 
- 	// CSS and Javascript enqueues.
-   require_once __DIR__ . '/functions/enqueues.php';
+	// CSS and Javascript enqueues.
+	require_once __DIR__ . '/functions/enqueues.php';
 
 
-   // CSS and Javascript enqueues.
-   require_once __DIR__ . '/functions/cleanup.php';
+	// Add specific cleanup files for theme.
+	require_once __DIR__ . '/functions/cleanup.php';
 
 
-   // CSS and Javascript enqueues.
-   require_once __DIR__ . '/functions/menus.php';
+	// Register menus.
+	require_once __DIR__ . '/functions/menus.php';
 
 
-   // CSS and Javascript enqueues.
-   require_once __DIR__ . '/functions/theme-support.php';
+	// Add theme support functions.
+	require_once __DIR__ . '/functions/theme-support.php';
 
 
- 	// CSS and Javascript enqueues.
-   require_once __DIR__ . '/functions/template-tags.php';
+	// Generate useful template tags.
+	require_once __DIR__ . '/functions/template-tags.php';

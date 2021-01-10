b15b2fe20130258e4f234e5574d1fc91f9d9d155
Iakovos Frountas <iakovosfrountas@gmail.com>
2021-01-10 19:09:15 +0200

    update on burger-menu

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,4 +1,4 @@
-import './function.bodyclasses.js';
+//import './function.bodyclasses.js';
 import './function.slideshow.js'; 
 import './function.offcanvas.js';
 //import './function.lazyload.js'; 
